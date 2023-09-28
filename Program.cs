c0543744bebb13ecb4737e80f9cf7e464a6b087d
yaaandir <haroldjimenez3708@gmail.com>
2023-09-28 00:30:36 -0600

    Funcion pares e impares

--- Program.cs
+++ Program.cs
@@ -5,17 +5,18 @@ class Program
 {
     static void Main()
     {
-        int opcion=0;
+        int opcion = 0;
         do
         {
-            Console.WriteLine("Menú de operaciones:");
+            Console.Clear(); // Limpiar la pantalla
+            Console.WriteLine("Bienvenido al menú de operaciones:");
             Console.WriteLine("1. Pares e Impares");
             Console.WriteLine("2. Tabla de multiplicar");
             Console.WriteLine("3. Factorial de un número");
             Console.WriteLine("4. Números Primos");
             Console.WriteLine("5. Sucesión de Fibonacci");
             Console.WriteLine("6. Salir");
-            Console.Write("Ingrese su elección (1-6): ");
+            Console.Write("Ingrese una opción del 1 al 6: ");
 
             string? input = Console.ReadLine();
 
@@ -51,6 +52,47 @@ class Program
                 Console.WriteLine("Entrada no válida. Por favor, ingrese un número del 1 al 6.");
             }
 
-        } while (opcion != 6);
+        } while (opcion != 6); // Continuar hasta que se seleccione la opción 6 (Salir)
+    }
+ // Implementación de las funciones:
+        static void ParesImpares()
+    {
+        do
+        {
+            Console.Clear(); // Limpiar la pantalla
+            Console.Write("Ingrese un número entero positivo para realizar las operaciones de pares e impares: ");
+            string? input = Console.ReadLine();
+            if (!string.IsNullOrEmpty(input) && int.TryParse(input, out int numero))
+            {
+                Console.WriteLine("Pares: ");
+                for (int i = 0; i <= numero; i++)
+                {
+                    if (i % 2 == 0)
+                        Console.Write(i + " ");
+                }
+                Console.WriteLine();
+                Console.WriteLine("Impares: ");
+                for (int i = 0; i <= numero; i++)
+                {
+                    if (i % 2 != 0)
+                        Console.Write(i + " ");
+                }
+                Console.WriteLine();
+
+                int sumaPares = Enumerable.Range(0, numero + 1).Where(x => x % 2 == 0).Sum();
+                int sumaImpares = Enumerable.Range(0, numero + 1).Where(x => x % 2 != 0).Sum();
+                Console.WriteLine($"Suma de pares: {sumaPares}");
+                Console.WriteLine($"Suma de impares: {sumaImpares}");
+                Console.WriteLine(sumaPares > sumaImpares ? "La suma de pares es mayor que la suma de impares." : "La suma de impares es mayor que la suma de pares.");
+            }
+            else
+            {
+                Console.WriteLine("Entrada no válida. Por favor, ingrese un número entero positivo.");
+            }
+
+            Console.Write("¿Desea realizar otra operación en Pares e Impares? (s/n): ");
+        } while (Console.ReadLine()?.Trim().ToLower() == "s");
+
+        Console.Clear(); // Limpiar la pantalla antes de salir de la función
     }
-} 
+}
